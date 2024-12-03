dbc8315f84b0ab7d2ecda81816e3a99e23c873fc
mostafa <mostafakhanderoo7@gmail.com>
2024-12-03 15:18:01 +0330

    refactor BaseEntity , add method save

---
+++ src/main/java/base/entity/BaseEntity.java
@@ -0,0 +1,17 @@
+package base.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseEntity<ID extends Serializable> {
+  private ID id;
+
+
+}
